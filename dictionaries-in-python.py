459702e9a2c2dfbd3158df5e2e37ea9e814d85af
masalakie <masalakiebangura27@gmail.com>
2024-08-22 16:36:46 -0400

    Add files via upload

---
+++ dictionaries-in-python.py
@@ -0,0 +1,21 @@
+
+product = {'path': 'red_getting_bread_python/README.md', 'size': 26}
+
+print(product)
+
+product = {'path': 'red_getting_bread_python/hello_world.py', 'size': 108}
+
+print(product)
+
+product = {'path': 'red_getting_bread_python/test_name.py', 'size': 68}
+
+print(product)
+
+product = {'path': 'red_getting_bread_python/dictionaries.py', 'size': 600}
+
+print(product)
+
+product = {'path': 'red_getting_bread_python/list.py', 'size': 594}
+
+print(product)
+
